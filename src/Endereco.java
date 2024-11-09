81de098b955954d6c77cb0156bff377a50e031bd
Gabriel Freire <gabrielmfreire2506@gmail.com>
2024-11-09 18:09:25 -0300

    Criação e desenvolvimento

---
+++ src/Endereco.java
@@ -0,0 +1,10 @@
+public record Endereco(String cep, String logradouro,
+                       String complemento, String bairro,
+                       String localidade, String uf) {
+
+    public void white(String json) {
+    }
+}
+
+
+
